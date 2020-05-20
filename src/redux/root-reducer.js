3063a681ccaffaa2cdb530b6ebd10ce8effa3b93
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-20 14:52:59 +0800

    moved our directory, and shop data into its own reducer, created respective selectors and updated corresponding components with redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,29 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// this is choosing local storage basically, you can also choose session storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
+
+// without redux persist it would just be
+// export default combineReducers({
+//   user: userReducer,
+//   cart: cartReducer,
+// });
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  shop => shop.collections
+);
+
+// Convert object to array for collection overview componen to select
+// need to look into the structure of shop.data to understand the structure
+// and then see collections-overview.component to see how it is being selected and rendered
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
