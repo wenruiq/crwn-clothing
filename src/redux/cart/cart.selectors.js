94f60c501d44f8683496f2f6d6c65e262aa32b72
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-20 09:38:55 +0800

    added checckout page, checkout item component, updated cart to use reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,8 +9,20 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce((accQty, cartItem) => accQty + cartItem.quantity, 0)
+);
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accQty, cartItem) => accQty + cartItem.quantity * cartItem.price,
+    0
+  )
 );
