63f17ff46e140843a34656738cb3f5ec34ace21c
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-23 14:43:24 +0800

    fixed typo of dirname

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__direname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   // * means for any route, do this function
   app.get('*', function (req, res) {
