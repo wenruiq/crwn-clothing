e4bd85eb562a117dd91dcf11d82a497056931ddc
wenruiq <58852708+wenruiq@users.noreply.github.com>
2020-05-25 12:52:15 +0800

    added compression libraries, memo, lazy suspense for optimizations

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component';
 
 // HOC, if not loading, then just render whatever wrapped inside normally
 const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,6 +11,8 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Compression is for optimization, gzip to smaller js chunks
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
